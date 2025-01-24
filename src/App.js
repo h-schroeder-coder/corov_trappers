c4871b002559861a07fe6c7534019058bed1e208
h-schroeder-coder <hankorschroeder@gmail.com>
2025-01-24 17:09:00 -0600

    Added image carousel and learn more button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import ImageCarousel from "./ImageCarousel";
 import Header from "./Header";
 import Footer from "./Footer";
 
@@ -8,6 +9,14 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <button
+        type="button"
+        className="btn btn-lg btn-border LearnMore-Button"
+        data-bs-toggle="button"
+      >
+        Learn More
+      </button>
+      <ImageCarousel />
       <Footer />
     </div>
   );
---
+++ src/ImageCarousel.js
@@ -0,0 +1,74 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import BeaversOverBoat from "./BeaversOverBoat.jpg";
+import BeaversinRound from "./BeaversinRound.jpg";
+import FursOnBarnWall from "./FursOnBarnWall.jpg";
+import BeaversHangingPinned from "./BeaversHangingPinned.jpg";
+import "./ImageCarousel.css";
+
+export default function ImageCarousel () {
+    return (
+      <div className="ImageCarousel container">
+        <div
+          id="carouselExampleAutoplaying"
+          class="carousel slide"
+          data-bs-ride="carousel"
+        >
+          <div className="carousel-inner w-80">
+            <div className="carousel-item active">
+              <img
+                src={BeaversinRound}
+                className="d-block w-80"
+                alt="Beavers, raccoons, and other animals laid out in a circle in the snow"
+              />
+            </div>
+            <div className="carousel-item">
+              <img
+                src={BeaversOverBoat}
+                className="d-block w-80"
+                alt="Beavers Lying on Boat"
+              />
+            </div>
+            <div className="carousel-item">
+              <img
+                src={FursOnBarnWall}
+                className="d-block w-80"
+                alt="Furs Hanging on Side of Barn"
+              />
+            </div>
+            <div className="carousel-item">
+              <img
+                src={BeaversHangingPinned}
+                className="d-block w-80"
+                alt="Beaver Furs Haning and Pelts Pinned in Background"
+              />
+            </div>
+          </div>
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExampleAutoplaying"
+            data-bs-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button
+            className="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExampleAutoplaying"
+            data-bs-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
+      </div>
+    );
+}
